27fef72b816856aa00c0bf69480b7666baab0e46
kurushiba <kurushiba55555@gmail.com>
2025-07-11 22:17:43 +0900

    initial commit

---
+++ src/modules/cards/card.entity.ts
@@ -0,0 +1,16 @@
+export class Card {
+  id!: string;
+  title!: string;
+  position!: number;
+  description!: string;
+  dueDate?: string;
+  completed!: boolean;
+  listId!: string;
+
+  constructor(data: Card) {
+    Object.assign(this, data);
+    if (data.dueDate != null) {
+      this.dueDate = new Date(data.dueDate).toLocaleDateString('sv-SE');
+    }
+  }
+}
